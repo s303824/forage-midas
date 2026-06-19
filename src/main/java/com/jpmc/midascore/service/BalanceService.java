4a5f7f66321b6f496289479d3a52f5e8ae2ab0f9
Sean Yang <60932078+s303824@users.noreply.github.com>
2026-06-19 17:55:39 -0400

    fixed error, API now working
    API parameter is a string, not a long. just needed to parse it

--- src/main/java/com/jpmc/midascore/service/BalanceService.java
+++ src/main/java/com/jpmc/midascore/service/BalanceService.java
@@ -3,7 +3,6 @@ package com.jpmc.midascore.service;
 import com.jpmc.midascore.component.DatabaseConduit;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Balance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BalanceService {
 
-    @Autowired
-    DatabaseConduit databaseConduit;
+    private final DatabaseConduit databaseConduit;
+    public BalanceService(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
 
-    @GetMapping("/balance")
-    public Balance getBalance(@RequestParam String stringId) {
-        long id = Long.parseLong(stringId);
-        try{
-            UserRecord user = databaseConduit.findById(id);
-            return new Balance(user.getBalance());
-        }catch (Exception e){
-            System.out.println("BalanceService could not find user, return balance set to 0");
+    @GetMapping(path = "/balance")
+    public Balance getBalance(@RequestParam  String userId) {
+        long id = Long.parseLong(userId);
+        UserRecord user = databaseConduit.findById(id);
+        if (user == null) {
+            System.out.println("User not found for id {" + id + "}");
             return new Balance(0);
         }
-    }
-}
+        return new Balance(user.getBalance());
+    }}
